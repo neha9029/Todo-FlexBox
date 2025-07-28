feda12a54ab33afc86455ad154a8fefb412596f6
neha9029 <nehanaik9029@gmail.com>
2025-07-28 15:06:04 -0600

    added todo task and initial css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [task, setTask] = useState("");
+  const [todoList, addToList] = useState([]);
+
+  function handleAddToList(task) {
+    if (task.trim() === "") return;
+
+    addToList((prev) => [...prev, task]);
+    setTask("");
+  }
+
+  return <Task task={task} setTask={setTask} onAdd={handleAddToList} />;
+}
+
+function Task({ task, setTask, onAdd }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="task-container">
+      <label className="task-label">Enter Task :</label>
+      <input
+        className="task-input"
+        type="text"
+        placeholder="Enter To do Task"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button className="task-button" onClick={() => onAdd(task)}>
+        Add Task
+      </button>
     </div>
   );
 }
-
-export default App;
